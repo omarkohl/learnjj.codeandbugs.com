73ef312b9da7e88836328bcfd011947826e4c094
Omar Kohl <omarkohl@posteo.net>
2024-12-04 20:16:28 +0100

    update button on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Ju Jutsu Tutorial
           </Link>
         </div>
       </div>
