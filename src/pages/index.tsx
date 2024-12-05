ab687c44f7a14c8cac8dd042bdd9cc60280ade2f
Omar Kohl <omarkohl@posteo.net>
2024-12-05 08:39:13 +0100

    update page title and description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,8 +32,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Hands-on tutorial for learning Ju Jutsu (jj), the version control system.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
