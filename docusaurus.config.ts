6711647d1a64edfbdbd96ee3eb8d0d185eadf45d
Omar Kohl <omarkohl@posteo.net>
2024-12-12 10:16:51 +0100

    design: add new social-card image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.jpg',
     navbar: {
       title: 'Ju Jutsu Tutorial',
       logo: {
